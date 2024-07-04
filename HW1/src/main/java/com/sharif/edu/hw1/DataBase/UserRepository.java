b1835f408b49acad8685be521186f9ecacd0b830
moeinEN <moenayatti@gmail.com>
2024-07-04 18:25:51 +0330

    Fixed some stuff

--- HW1/src/main/java/com/sharif/edu/hw1/DataBase/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sharif.edu.hw1.DataBase;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
