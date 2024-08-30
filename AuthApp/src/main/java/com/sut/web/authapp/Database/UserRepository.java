7d67af45935573112a48c499bcb724fc3b687ea0
ParsaEnayatiCe <p.enayati0990@yahoo.com>
2024-08-30 19:43:28 +0330

    Final Commit

--- AuthApp/src/main/java/com/sut/web/authapp/Database/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sut.web.authapp.Database;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
